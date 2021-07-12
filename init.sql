9e871df0f87825a9fc0791598c0b0c352038515f
MigSer13 <avto470@mail.ru>
2021-07-13 00:34:48 +0300

    update sql

--- init.sql
+++ init.sql
@@ -1,17 +1,17 @@
 DROP TABLE IF EXISTS products CASCADE;
-CREATE TABLE products (id bigint PRIMARY KEY, title VARCHAR(255), price int);
+CREATE TABLE products (id bigserial PRIMARY KEY, title VARCHAR(255), price int);
 INSERT INTO products (title, price) VALUES
 ('tomato', 17),
 ('apple', 13),
 ('bread', 40);
 
 DROP TABLE IF EXISTS customers CASCADE;
-CREATE TABLE customers (id bigint PRIMARY KEY, title VARCHAR(255));
+CREATE TABLE customers (id bigserial PRIMARY KEY, title VARCHAR(255));
 INSERT INTO customers (title) VALUES
-('Modern'),
-('Sprut'),
-('Porto'),
-('Dorina');
+('OOO_Modern'),
+('OOO_Sprut'),
+('OOO_Porto'),
+('IP_Dorina');
 
 DROP TABLE IF EXISTS products_customer CASCADE;
 CREATE TABLE products_customer (product_id bigint, customer_id bigint, FOREIGN KEY (product_id) REFERENCES products (id), FOREIGN KEY (customer_id) REFERENCES customers (id));
--- src/main/java/ru/geekbrains/hib/MainApp.java
+++ src/main/java/ru/geekbrains/hib/MainApp.java
@@ -2,33 +2,53 @@ package ru.geekbrains.hib;
 
 import org.hibernate.Session;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MainApp {
     public static void main(String[] args) {
         MySessionFactory mySessionFactory = new MySessionFactory("hibernate.cfg.xml");
         ProductDAO productDAO = new ProductDAO(mySessionFactory.getSessionFactory());
 
-        Product p1 = new Product("Cucumber", 13);
-        productDAO.saveOrUpdate(p1);
-        Product p2 = new Product("Tomato", 17);
-        productDAO.saveOrUpdate(p2);
-
         Session session = null;
-        //
-        session = (Session) mySessionFactory.getSessionFactory();
-        session.beginTransaction();
-        Customer customer1 = session.get(Customer.class, 1);
-        List<Product> productsCustomer1 = customer1.getProductsList();
-        System.out.println(productsCustomer1);
-        session.getTransaction().commit();
-        //
-        session = (Session) mySessionFactory.getSessionFactory();
-        session.beginTransaction();
-        Product product2 = session.get(Product.class, 2);
-        List<Customer> customersOfProduct = product2.getCustomer();
-        System.out.println(customersOfProduct);
-        session.getTransaction().commit();
+        try {
+            String sql = Files.lines(Paths.get("init.sql")).collect(Collectors.joining(" "));
+            session = mySessionFactory.getSessionFactory().getCurrentSession();
+            session.beginTransaction();
+            session.createNativeQuery(sql).executeUpdate();
+            session.getTransaction().commit();
 
+
+//            Product p1 = new Product("Cucumber", 13);
+//            productDAO.saveOrUpdate(p1);
+//            Product p2 = new Product("Tomato", 17);
+//            productDAO.saveOrUpdate(p2);
+            //
+            session = (Session) mySessionFactory.getSessionFactory();
+            session.beginTransaction();
+            Customer customer1 = session.get(Customer.class, 1);
+            List<Product> productsCustomer1 = customer1.getProductsList();
+            System.out.println(productsCustomer1);
+            session.getTransaction().commit();
+            //
+            session = (Session) mySessionFactory.getSessionFactory();
+            session.beginTransaction();
+            Product product2 = session.get(Product.class, 2);
+            List<Customer> customersOfProduct = product2.getCustomer();
+            System.out.println(customersOfProduct);
+            session.getTransaction().commit();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            mySessionFactory.getSessionFactory().close();
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 }
